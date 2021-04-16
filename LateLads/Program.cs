0b69ff3e08e6db1feaf979357e17b62ccfa746cb
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-04-16 18:38:16 +0100

    Add project files.

---
+++ LateLads/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateLads
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string gender;
+            int age;
+            Console.WriteLine("What is your gender?: ");
+            gender = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your age?: ");
+            age = int.Parse(Console.ReadLine());
+
+            float premium = CalcPremium(age, gender);
+
+            Console.WriteLine("Your premium is {0}", premium);
+            Console.ReadLine();*/
+        }
+
+        public float CalcPremium(int age, string gender)
+        {
+            float premium = 0;
+
+            if (gender == "female")
+            {
+
+                if ((age >= 18) && (age <= 30))
+                {
+                    premium = 5.0F;
+                }
+                else if (age >= 31)
+                {
+                    premium = 3.50F;
+                }
+                else
+                {
+                    premium = 0.0F;
+                }
+            }
+            else if (gender == "male")
+            {
+
+                if ((age >= 18) && (age <= 35))
+                {
+                    premium = 6.0F;
+                }
+                else if (age >= 36)
+                {
+                    premium = 5.00F;
+                }
+                else
+                {
+                    premium = 0.0F;
+                }
+            }
+
+            if (age >= 50)
+            {
+                premium = premium * 0.5F;
+
+            }
+            return premium;
+
+        }
+    }
+
+}
---
+++ LateLadsTest/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LateLads;
+using NUnit.Framework;
+
+namespace LateLadsTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        static float myExpectedResult;
+        static Program p1;
+        
+        [SetUp]
+        public static void Init()
+        {
+            p1 = new Program();
+        }
+
+
+
+        [Test]
+        public static void Premium_For_Male1()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 0.0f;
+            float actualResult = p1.CalcPremium(16, "male");
+
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Male2()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 6.0f;
+            float actualResult = p1.CalcPremium(20, "male");
+
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Male3()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 5.0f;
+            float actualResult = p1.CalcPremium(36, "male");
+
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Male4()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 2.5f;
+            float actualResult = p1.CalcPremium(51, "male");
+
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Female1()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 0.0f;
+            float actualResult = p1.CalcPremium(12, "female");
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Female2()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 5.0f;
+            float actualResult = p1.CalcPremium(26, "female");
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Female3()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 3.5f;
+            float actualResult = p1.CalcPremium(35, "female");
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+        [Test]
+        public static void Premium_For_Female4()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 1.75f;
+            float actualResult = p1.CalcPremium(90, "female");
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+
+        [Test]
+        public static void Premium_Error()
+        {
+            // Arrange, Act, Assert
+            myExpectedResult = 0.0f;
+            float actualResult = p1.CalcPremium(20, "femla");
+            Assert.AreEqual(myExpectedResult, actualResult);
+        }
+    }
+}
